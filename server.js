449b57f58f161d1e04bb29f9402dde68736ece2e
Ashutosh Ranjan <72221793+ashutoshranjan-cmd@users.noreply.github.com>
2025-03-19 02:49:04 +0530

    changes

--- server.js
+++ server.js
@@ -81,8 +81,18 @@ app.use(
   })
 );
 
-// Handle preflight requests
-app.options("*", cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 
 app.use(cookieParser());
 app.use(express.json());
