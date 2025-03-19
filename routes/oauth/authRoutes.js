64b639eb3f0604e54c92ab5e502001b3b46b3da4
Ashutosh Ranjan <72221793+ashutoshranjan-cmd@users.noreply.github.com>
2025-03-19 14:01:56 +0530

    link change

--- routes/oauth/authRoutes.js
+++ routes/oauth/authRoutes.js
@@ -21,7 +21,7 @@ router.get(
     res.cookie("token", token, {
       httpOnly: true,
       secure: false, // Change to true if using HTTPS
-      sameSite: "Strict",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
