3978fbff758114a44dd4f083a108ca834b972c55
Ashutosh Ranjan <72221793+ashutoshranjan-cmd@users.noreply.github.com>
2025-03-19 10:01:06 +0530

    link change

--- controllers/oauth/passport.js
+++ controllers/oauth/passport.js
@@ -54,7 +54,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `https://shop-vibe-server.onrender.com//api/oauth/google/callback`,
+      callbackURL: `https://shop-vibe-server.onrender.com/api/oauth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
